a04ea3328a8edd349575d6f896cd8d7ed029849e
JYG <jojeayun900@gmail.com>
2022-11-16 21:12:25 +0900

    edit weatherInf & right_sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -7,31 +7,60 @@ function App() {
   return (
     <div className="App">
       <div css={S.MainPage}>
-        <div className='menubar'>
-
-        </div>
-        <div className='sidebar_left'>
-
-        </div>
         <div css={S.sidebar_right}>
           <div css={S.nowarea}>
-            <p css={S.areaname}>현재 지역: 광주광역시 광산구</p>
+            <p css={S.areaname}>현재 지역: %nowArea%</p>
           </div>
           <div css={S.forecast}>
             <div css={S.wrapper_f}>
               <div css={S.listbox_f}>
-                <p css={S.timeline}>2022.11.01 6:00</p>
-                <p>날씨: 비</p>
-                <p>습도: 40%</p>
-                <p>온도: 15°C</p>
-                <p>풍속: 15m/s</p>
+                <p css={S.timeline}>0000.00.00 0:00</p>
+                <p>날씨: </p>
+                <p>습도: 0%</p>
+                <p>온도: 0°C</p>
+                <p>풍속: 0m/s</p>
                 <div css={S.magnify}><img alt='magnify' src='/magnifying.png' /></div>
               </div>
-              <div css={S.listbox_f}></div>
-              <div css={S.listbox_f}></div>
-              <div css={S.listbox_f}></div>
-              <div css={S.listbox_f}></div>
-              <div css={S.listbox_f}></div>
+              <div css={S.listbox_f}>
+                <p css={S.timeline}>0000.00.00 0:00</p>
+                <p>날씨: </p>
+                <p>습도: 0%</p>
+                <p>온도: 0°C</p>
+                <p>풍속: 0m/s</p>
+                <div css={S.magnify}><img alt='magnify' src='/magnifying.png' /></div>
+              </div>
+              <div css={S.listbox_f}>
+                <p css={S.timeline}>0000.00.00 0:00</p>
+                <p>날씨: </p>
+                <p>습도: 0%</p>
+                <p>온도: 0°C</p>
+                <p>풍속: 0m/s</p>
+                <div css={S.magnify}><img alt='magnify' src='/magnifying.png' /></div>
+              </div>
+              <div css={S.listbox_f}>
+                <p css={S.timeline}>0000.00.00 0:00</p>
+                <p>날씨: </p>
+                <p>습도: 0%</p>
+                <p>온도: 0°C</p>
+                <p>풍속: 0m/s</p>
+                <div css={S.magnify}><img alt='magnify' src='/magnifying.png' /></div>
+              </div>
+              <div css={S.listbox_f}>
+                <p css={S.timeline}>0000.00.00 0:00</p>
+                <p>날씨: </p>
+                <p>습도: 0%</p>
+                <p>온도: 0°C</p>
+                <p>풍속: 0m/s</p>
+                <div css={S.magnify}><img alt='magnify' src='/magnifying.png' /></div>
+              </div>
+              <div css={S.listbox_f}>
+                <p css={S.timeline}>0000.00.00 0:00</p>
+                <p>날씨: </p>
+                <p>습도: 0%</p>
+                <p>온도: 0°C</p>
+                <p>풍속: 0m/s</p>
+                <div css={S.magnify}><img alt='magnify' src='/magnifying.png' /></div>
+              </div>
             </div>
             <div css={S.wrapper_d}>
               <div css={S.detailbox}>
@@ -40,8 +69,22 @@ function App() {
             </div>
           </div>
         </div>
-        <div className='weatherInf'>
-
+        <div css={S.weatherInf}>
+          <div css={S.weatherIcon}></div>
+          <div css={S.nowTime}><p>오전 0:00</p></div>
+          <div css={S.weatherBox}>
+            <h1 css={S.nowWeather}>맑음 / 0°C</h1>
+            <p css={S.weatherState}>오늘은 화창해요.</p>
+          </div>
+          <div css={S.tipArea}>
+            <div css={S.tipBox}><p>wether_tip_1</p></div>
+            <div css={S.tipBox}><p>wether_tip_2</p></div>
+            <div css={S.tipBox}><p>wether_tip_3</p></div>
+            <div css={S.tipBox}><p>wether_tip_4</p></div>
+          </div>
+        </div>
+        <div>
+          <img css={S.sideicon} alt='sidemenubar' src='/sidemenubar.png' />
         </div>
       </div>
     </div>
